743322a229ea8c0d794d9f49f5c122328fe05d29
ntsh-oni <teamnutshell.dev@gmail.com>
2025-08-15 13:54:10 +0200

    steam init

--- src/ntshengn_platform_module.cpp
+++ src/ntshengn_platform_module.cpp
@@ -1,13 +1,28 @@
 #include "ntshengn_platform_module.h"
+#include "../Module/utils/ntshengn_dynamic_library.h"
+#include "../Module/utils/ntshengn_module_defines.h"
+#include "../external/sdk/public/steam/steam_api.h"
 
 void NtshEngn::PlatformModule::init() {
-
+	if (!SteamAPI_Init()) {
+		NTSHENGN_MODULE_ERROR("Steam must be running to play this game.");
+	}
 }
 
 void NtshEngn::PlatformModule::update(float dt) {
 	NTSHENGN_UNUSED(dt);
+
+	SteamAPI_RunCallbacks();
 }
 
 void NtshEngn::PlatformModule::destroy() {
+	SteamAPI_Shutdown();
+}
 
+extern "C" NTSHENGN_MODULE_API NtshEngn::PlatformModuleInterface* createModule() {
+	return new NtshEngn::PlatformModule;
+}
+
+extern "C" NTSHENGN_MODULE_API void destroyModule(NtshEngn::PlatformModuleInterface* m) {
+	delete m;
 }
