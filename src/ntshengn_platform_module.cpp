92522a561334dce4d12048760a0700d628148b85
ntsh-oni <teamnutshell.dev@gmail.com>
2025-08-15 14:20:49 +0200

    add create and destroy module functions

--- src/ntshengn_platform_module.cpp
+++ src/ntshengn_platform_module.cpp
@@ -1,13 +1,24 @@
 #include "ntshengn_platform_module.h"
+#include "../Module/utils/ntshengn_dynamic_library.h"
+#include "../Module/utils/ntshengn_module_defines.h"
 
 void NtshEngn::PlatformModule::init() {
-
+    NTSHENGN_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 void NtshEngn::PlatformModule::update(float dt) {
 	NTSHENGN_UNUSED(dt);
+    NTSHENGN_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 void NtshEngn::PlatformModule::destroy() {
+    NTSHENGN_MODULE_FUNCTION_NOT_IMPLEMENTED();
+}
 
+extern "C" NTSHENGN_MODULE_API NtshEngn::PlatformModuleInterface* createModule() {
+    return new NtshEngn::PlatformModule;
+}
+
+extern "C" NTSHENGN_MODULE_API void destroyModule(NtshEngn::PlatformModuleInterface* m) {
+    delete m;
 }
